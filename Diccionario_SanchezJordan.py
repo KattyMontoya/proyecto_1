a0ea7a77bea9c673a337789e539dd0a729adc326
USUARIO13-PC\Usuario01 <jordan_jssh_@hotmail.com>
2016-11-16 13:45:17 -0500

    Diccionario Sanchez Jordan

---
+++ Diccionario_SanchezJordan.py
@@ -0,0 +1,14 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> diccionario={'Jordan': 123, 'Jorge': 456, 'Edison':789, 'Anahi': 159, 'Kathy': 753}
+>>> diccionario['Anahi']
+159
+>>> list(diccionario.keys())
+['Anahi', 'Jorge', 'Edison', 'Kathy', 'Jordan']
+>>> sorted(diccionario.keys())
+['Anahi', 'Edison', 'Jordan', 'Jorge', 'Kathy']
+>>> 'Jordan' in diccionario
+True
+>>> 'Adrian' in diccionario
+False
+>>> 
