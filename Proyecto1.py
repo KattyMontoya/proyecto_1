37d4445e267d110e68e782a058eb186c43d1cabe
DESKTOP-FF1JBVS\jorda <jordan_jssh_ @hotmail.com>
2016-11-22 22:26:31 -0500

    Realice un bucle de repeticion para que el usuario decida cuando terminar el programa, además cambie los tipos de variables de "int" a "float" para que el usuario pueda ingresar valores decimales o enteros. Por ultimo valide las opciones, es decir, si el usuario digita una opcion que no esta en el menu el programa le retorna un mensaje de error.

--- Proyecto1.py
+++ Proyecto1.py
@@ -3,8 +3,10 @@
 ## Autores: Campoverde Jorge - Montoya Katherine - Sanchez Jordan
 ## Fecha: 23-Nov-2016
 
+import os
+
 def menu ():
-    print("\t\t\t**CALCULO DE PERIMETROS Y AREAS")
+    print("\t\t\t**CALCULO DE PERIMETROS Y AREAS**")
     print("\n-OPCIONES:")
     print("1.- Circulo")
     print("2.- Triangulo")
@@ -14,7 +16,7 @@ def menu ():
 
 def circulo():
     pi=3.1415
-    radio=int(input("\nINGRESE EL VALOR DEL RADIO"))
+    radio=float(input("\nINGRESE EL VALOR DEL RADIO: "))
     a= 2*pi*radio
     p= pi*radio**2
     print ("\nEl valor del perimetro es: ",p)
@@ -22,24 +24,27 @@ def circulo():
     
 
 def triangulo():
-    x=int (input("Ingrese el valor de los lados:"))
-    h=int (input("Ingrese el valor de la altura:"))
+    x=float (input("\nIngrese el valor de los lados: "))
+    h=float (input("Ingrese el valor de la altura: "))
     a=(x*h)/2
     p=x+x+x
     print ("\nEl valor del perimetro es: ",p)
     print ("El valor del area es: ",a)
 
 def cuadrado():
-    l=int(input("Igrese el valor del lado: "))
+    l=float(input("\nIgrese el valor del lado: "))
     a=l*l 
     p=l+l+l+l
     print ("\nEl valor del perimetro es: ",p)
     print ("El valor del area es: ",a)
     
 def poligonos_r():
-    n=int(input("Ingrese el numerode lados:"))
-    l=int(input("Ingrese la medida de uno de los lados:"))
-    a=int(input("Ingrese la medida del apotema:"))
+    n=float(input("Ingrese el numerode lados: "))
+    l=float(input("Ingrese la medida de uno de los lados: "
+
+
+                ))
+    a=float(input("Ingrese la medida del apotema: "))
     
     if n>=5 or n<=10:
         a=(n*l*a)/2
@@ -53,15 +58,23 @@ def poligonos_r():
     
 def main():
     menu()
-    opc=int(input("\nESCOJA UNA OPCION:"))
-    if opc==1:
-        circulo()
-    if opc==2:
-        triangulo()
-    if opc==3:
-        cuadrado()
-    if opc==4:
-        poligonos_r()
-
-    
+    a='a'
+    opc=input("\nESCOJA UNA OPCION(PARA TERMINAR EL ""\n""PROGRAMA NO DIJITE NADA Y PRESIONE ENTER): ")
+    while opc !="":
+        if opc==1:
+            circulo()
+        if opc==2:
+            triangulo()
+        if opc==3:
+            cuadrado()
+        if opc==4:
+            poligonos_r()
+        else:
+            print("\nOPCION INCORRECTA... INTENTE DE NUEVO")
+        a=input("\nPRESIONE ENTER PARA CONTINUAR...")
+        if a=="":
+            os.system("cls")
+        menu()
+        opc=int(input("\nESCOJA UNA OPCION(PARA TERMINAR EL ""\n""PROGRAMA NO DIJITE NADA Y PRESIONE ENTER): "))
+    print("\nFIN DEL PROGRAMA..!!")   
 main()
